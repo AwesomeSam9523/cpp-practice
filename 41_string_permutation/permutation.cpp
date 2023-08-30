59c9bbef9242548288572f0e86a6fd08f03428ff
AwesomeSam9523 <samakshgupta04@gmail.com>
2023-08-30 14:50:06 +0530

    Switch to user input

--- 41_string_permutation/permutation.cpp
+++ 41_string_permutation/permutation.cpp
@@ -4,8 +4,9 @@ using namespace std;
 bool isPermutation(char input1[], char input2[]);
 
 int main() {
-    char input1[] = "abc";
-    char input2[] = "abcc";
+    char input1[100];
+    char input2[100];
+    cin >> input1 >> input2;
     bool result = isPermutation(input1, input2);
     cout << result << endl;
     return 0;
@@ -13,15 +14,22 @@ int main() {
 
 bool isPermutation(char input1[], char input2[]) {
     int finalSum = 0;
+    bool one = false, two = false;
     for (int i = 0;; i++) {
-        if (
-            (input2[i] == '\0' && input1[i] != '\0') ||
-            (input2[i] != '\0' && input1[i] == '\0')
-        ) {
-            return false;
+        // cout << i << " " << input1[i] << " " << input2[i] << endl;
+
+        if (input1[i] == '\0') {
+            one = true;
+        }
+        if (input2[i] == '\0') {
+            two = true;
+        }
+        if (one && two) {
+            break;
         }
         finalSum += input1[i] - input2[i];
     }
+
     if (finalSum == 0) {
         return true;
     } else {
